638c9739ffcba84b6791702de2150d878ec1d8f1
Vivek Raina <87175291+vivekraina7@users.noreply.github.com>
2025-09-03 23:58:47 +0530

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,12 +7,12 @@ export const loaderAnimation = [
 ]
 
 export const LINKS = {
-  github: 'https://github.com/bue221',
-  linkedin: 'https://www.linkedin.com/in/bue221/',
-  mail: 'mailto:camiloplaza3@gmail.com',
-  instagram: 'https://www.instagram.com/caj_ink/',
-  medium: 'https://ladvace.medium.com/',
-  discord: 'https://discordapp.com/users/163300027618295808',
+  github: 'https://github.com/vivekraina7',
+  linkedin: 'https://www.linkedin.com/in/vivekrainaa/',
+  mail: 'mailto:vivekraina33.vr@gmail.com',
+  instagram: 'https://www.instagram.com/viren.raina/',
+  medium: 'https://www.linkedin.com/in/vivekrainaa/',
+  discord: 'https://www.linkedin.com/in/vivekrainaa/',
 }
 
 // Global
@@ -51,110 +51,61 @@ export const SEARCH: Page = {
 export const STUDIES = [
   {
     title: 'Computer Science',
-    institution: 'Universidad Central',
-    link: 'https://www.ucentral.edu.co/',
+    institution: 'Graphic Era University',
+    link: 'https://gehu.ac.in/',
     date: '2019 - 2027',
   },
   {
-    title: '...',
-    institution: 'Platzi',
-    link: 'https://platzi.com/',
+    title: 'X',
+    institution: 'Shiksha Niketan',
+    link: 'https://shikshaniketanjk.in/',
     date: '2018 - 2022',
   },
   {
-    title: '...',
-    institution: 'Udemy',
-    link: 'https://www.udemy.com/',
+    title: 'XII',
+    institution: 'Shiksha Niketan',
+    link: 'https://shikshaniketanjk.in/',
     date: '2018 - 2022',
-  },
-  {
-    title: '...',
-    institution: 'Youtube',
-    link: 'https://www.youtube.com/@midulive',
-    date: '2018 - 2022',
-  },
-  {
-    title: 'Tattoo Artist',
-    institution: 'Tattoo Academy',
-    link: 'https://www.instagram.com/tattoodcacademy/',
-    date: '2024 - 2025',
-  },
+  }
 ]
 
 export const EXPERIENCE = [
   {
-    company: 'Straico',
-    location: 'Bogotá D C, Colombia',
-    position: 'Software Engineer',
-    start: '2021',
-    link: 'https://straico.com/',
+    company: 'Qriocity Ventures',
+    location: 'Chennai, India',
+    position: 'ML/AI Engineer',
+    start: 'Jan 2024',
+    link: 'https://www.qriocity.in/',
     end: 'Current',
     tasks: [
-      'Integration IA services with react and tailwind css',
-      'Development and build of DB with mongoDB',
+      'Led a cross-functional team of 7–10 ML and full-stack developers for AI-powered solutions.',
+      'Built YOLO & Custom models for real-time object detection and vision tasks.',
+      'Developed HuggingFace-based NLP models for domain-specific applications.',
+      'Delivered projects in retail analytics (shelf monitoring) and finance (OCR parsing).',
     ],
   },
   {
-    company: 'Spot2',
-    location: 'Mexico City, Mexico',
-    position: 'Software Engineer',
-    link: 'https://spot2.mx/',
-    start: '2021',
-    end: 'Current',
+    company: 'Rellins',
+    location: 'Chandigarh, India',
+    position: 'AI Engineer',
+    link: 'https://relinns.com/',
+    start: 'May 2023',
+    end: 'Oct 2023',
     tasks: [
-      'Development of the Spot2 platform with the use of React, Redux, and Material UI',
-      'Development map with the use of Google Maps API',
-      'Development internal platform with the use of React, Redux, and Material UI',
-      'Testing and debugging',
+      'Created a chatbot using ChatGPT-4, LlamaIndex, and Falcon for user support.',
+      'Fine-tuned NLP models to boost chatbot performance and efficiency.'
     ],
   },
   {
-    company: 'Imaginamos',
-    link: 'https://imaginamos.com/',
-    location: 'Bogotá D C, Colombia',
-    position: 'Frontend developer',
-    start: '2021',
-    end: '2021',
+    company: 'Omdana',
+    link: 'https://www.omdena.com/',
+    location: 'Remote - Omdana Berlin Chapter',
+    position: 'ML developer',
+    start: 'April 2023',
+    end: ' May 2023',
     tasks: [
       'Development of the Imaginamos platform with the use of React, Redux, and Material UI',
       'Work in ETB project with the use of React, Redux, and Material UI',
     ],
-  },
-  {
-    company: 'INETUM',
-    location: 'Bogotá D C, Colombia',
-    position: 'Frontend developer',
-    start: '2021',
-    link: 'https://www.inetum.com/es',
-    end: '2021',
-    tasks: [
-      'Support in the QA area and bug review',
-      'Use of SCRUM methodology',
-      'Claro projects with the use of frameworks and libraries such as react and angular',
-      'Use of redux toolkit as aproposal which allowed a shorter development time when using this tool',
-    ],
-  },
-  {
-    company: 'S I G',
-    location: 'Bogotá D C, Colombia',
-    position: 'Software developer',
-    start: '2021',
-    end: '2021',
-    tasks: [
-      'Development of a dashboard with react admin and chartjs for managing and entering information',
-    ],
-  },
-  {
-    company: 'BOOKII',
-    location: 'Bogotá D C, Colombia',
-    position: 'Software developer',
-    start: '2019',
-    end: '2021',
-    tasks: [
-      'Productive collaborationwiththebackendteamforthecreationofthe conversationalclubs',
-      'Effective coding of conversational clubs following design guide lines and using the Redux statemanager',
-      'Creation of the Bookii page in Spanish and change of texts in cms',
-      'Generation of static posts using Gatsby and the Contentful cms for the Bookii blog',
-    ],
-  },
+  }
 ]
